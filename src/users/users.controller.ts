c5d9255274f87951f63c3d34a411defef29792f2
Er Santosh <er.santoshtharu@gmail.com>
2022-06-10 22:01:31 +0545

    Creating and saving user data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,43 @@
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOneById(parseInt(id));
+  }
+
+  @Get()
+  findUserByEmail(@Query('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
